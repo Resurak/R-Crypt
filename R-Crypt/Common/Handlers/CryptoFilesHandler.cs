18769748fd41ae5a7f2f9e5b5ffeefffe4c01735
Resurak <daniele.lopresti97@gmail.com>
2020-12-24 16:18:49 +0100

    better threading. better error handling

--- R-Crypt/Common/Handlers/CryptoFilesHandler.cs
+++ R-Crypt/Common/Handlers/CryptoFilesHandler.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,108 +21,126 @@ namespace R_Crypt.Common.Handlers
 {
     public class CryptoFilesHandler : BaseModel
     {
-        public void GetCryptoFiles(string[] files, bool overwrite)
+        private bool IsUpdatingCollection = false;
+
+        public async void GetCryptoFiles(string[] files, bool overwrite)
         {
             if (overwrite && CryptoFiles.Count > 0) CryptoFiles.Clear();
 
-            foreach (var file in files)
+            IsUpdatingCollection = true;
+
+            if (files != null)
             {
-                CryptoFile crypto = new();
-
-                if (file.CheckExist())
+                foreach (var file in files)
                 {
-                    crypto.Bool_IsFolder = file.IsDirectoryOrFile();
+                    CryptoFile crypto = new();
 
-                    crypto.Str_Path = file;
-                    crypto.Str_FileName = crypto.Str_Path.GetFileName();
-
-                    crypto.Tooltip = $"Path of the file: {crypto.Str_Path}";
-
-                    if (crypto.Bool_IsFolder)
+                    if (!string.IsNullOrEmpty(file))
                     {
-                        crypto.Icon = new BitmapImage(new Uri(@"pack://application:,,,/"
-                            + Assembly.GetExecutingAssembly().GetName().Name
-                            + ";component/"
-                            + "Resources/folder.png", UriKind.Absolute));
-
-                        crypto.Long_FileSize = 0;
-                        crypto.Str_FileSize = "";
-                        crypto.Str_FileType = "Folder";
-                    }
-                    else
-                    {
-                        FileInfo info = new(file);
-
-                        crypto.Long_FileSize = info.Length;
-
-                        crypto.Str_FileSize = info.Length.ConvertLongByteToString();
-                        crypto.Str_FileExtension = file.GetFileExtension();
-                        crypto.Str_FileType = file.GetFileExtension().GetExtensionType();
-                    }
-
-                    crypto.Long_ProcessedSize = 0;
-                    crypto.Str_ProgressTracker = "Waiting input";
-
-                    crypto.Vis_ProgressBar = Visibility.Hidden;
-                    crypto.Vis_TextBlock = Visibility.Visible;
-                }
-                else
-                {
-                    crypto.Str_FileName = "Error";
-                    crypto.Tooltip = $"Error. Cannot get {crypto.Str_Path}. Probably need admin privileges";
-                    crypto.Error = true;
-                }
-
-                CryptoFiles.Add(crypto);
-
-                OnCollectionChanged();
-            }
-
-            AddIcons();
-        }
-
-        private async void AddIcons()
-        {
-            foreach (var file in CryptoFiles)
-            {
-                if (!file.Bool_IsFolder)
-                {
-                    var worker = new BackgroundWorker();
-                    worker.DoWork += (sender, e) =>
-                    {
-                        using (Icon ico = Icon.ExtractAssociatedIcon(file.Str_Path))
+                        if (file.CheckExist())
                         {
-                            ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(
-                                    ico.Handle,
-                                    Int32Rect.Empty,
-                                    BitmapSizeOptions.FromEmptyOptions());
+                            crypto.Bool_IsFolder = file.IsDirectoryOrFile();
 
-                            imageSource.Freeze();
+                            crypto.Str_Path = file;
+                            crypto.Str_FileName = crypto.Str_Path.GetFileName();
 
-                            file.Icon = imageSource;
+                            crypto.Tooltip = $"Path of the file: {crypto.Str_Path}";
+
+                            if (crypto.Bool_IsFolder)
+                            {
+                                crypto.Icon = new BitmapImage(new Uri(@"pack://application:,,,/"
+                                    + Assembly.GetExecutingAssembly().GetName().Name
+                                    + ";component/"
+                                    + "Resources/folder.png", UriKind.Absolute));
+
+                                crypto.Long_FileSize = 0;
+                                crypto.Str_FileSize = "";
+                                crypto.Str_FileType = "Folder";
+                            }
+                            else
+                            {
+                                FileInfo info = new(file);
+
+                                crypto.Long_FileSize = info.Length;
+
+                                crypto.Str_FileSize = info.Length.ConvertLongByteToString();
+                                crypto.Str_FileExtension = file.GetFileExtension();
+                                crypto.Str_FileType = file.GetFileExtension().GetExtensionType();
+                            }
+
+                            crypto.Long_ProcessedSize = 0;
+                            crypto.Str_ProgressTracker = "Waiting input";
+
+                            crypto.Vis_ProgressBar = Visibility.Hidden;
+                            crypto.Vis_TextBlock = Visibility.Visible;
                         }
-                    };
-                    worker.RunWorkerAsync();
+                        else
+                        {
+                            crypto.Str_FileName = "Error";
+                            crypto.Tooltip = $"Error. Cannot get {crypto.Str_Path}. Probably need admin privileges";
+                            crypto.Error = true;
+                        }
+
+                        CryptoFiles.Add(crypto);
+                    }
+                    else CryptoFiles.Add(new CryptoFile { Str_FileName = "Error", Error = true });
                 }
-                await Task.Run(() => Thread.Sleep(10));
             }
+            else throw new Exception("Files are null");
+
+            OrderCollectionByFolder();
+            OnCollectionChanged(overwrite);
         }
 
-        private void OnCollectionChanged()
+        private async void OnCollectionChanged(bool overwrite)
         {
             Int_TotalFiles = CryptoFiles.Count;
+
+            Long_TotalSize = 0;
+            Long_ProcessedSize = 0;
 
             int folders = 0;
 
             foreach (var file in CryptoFiles)
             {
-                Long_TotalSize += file.Long_FileSize;
-                Long_ProcessedSize += file.Long_ProcessedSize;
+                if (!file.Error)
+                {
+                    Long_TotalSize += file.Long_FileSize;
+                    Long_ProcessedSize += file.Long_ProcessedSize;
 
-                if (file.Bool_IsFolder) folders++;
+                    if (file.Bool_IsFolder) folders++;
+                }
             }
 
-            if (folders > 0) Int_TotalFolders = folders; 
+            if (folders > 0) Int_TotalFolders = folders;
+
+            foreach (var file in CryptoFiles)
+            {
+                if (!file.Error && !file.Bool_IsFolder)
+                {
+                    await Task.Run(() =>
+                    {
+                        using (Icon ico = Icon.ExtractAssociatedIcon(file.Str_Path))
+                        {
+                            file.Icon = ico.ToImageSource();
+                        }
+                    });
+                }
+            }
+
+            IsUpdatingCollection = false;
+        }
+
+        private void OrderCollectionByFolder()
+        {
+            ObservableCollection<CryptoFile> FolderFirst =
+                new ObservableCollection<CryptoFile>(CryptoFiles.Where(folderName => folderName.Bool_IsFolder).OrderBy(folderName => folderName.Str_FileName).ToList());
+
+            ObservableCollection<CryptoFile> FileSeconds =
+                new ObservableCollection<CryptoFile>(CryptoFiles.Where(fileName => !fileName.Bool_IsFolder).OrderBy(fileName => fileName.Str_FileName).ToList());
+
+            CryptoFiles = new ObservableCollection<CryptoFile>(FolderFirst.Concat(FileSeconds).ToList());
+
         }
 
 
@@ -144,7 +163,7 @@ namespace R_Crypt.Common.Handlers
         public TimeSpan Time_Remaining { get => _Time_Remaining; set { _Time_Remaining = value; Notify(); } }
         private TimeSpan _Time_Remaining;
 
-        public ObservableCollection<CryptoFile> CryptoFiles { get => _CryptoFiles; set { _CryptoFiles = value; OnCollectionChanged(); Notify(); } }
+        public ObservableCollection<CryptoFile> CryptoFiles { get => _CryptoFiles; set { _CryptoFiles = value; Notify(); } }
         private ObservableCollection<CryptoFile> _CryptoFiles = new();
 
 
@@ -174,11 +193,14 @@ namespace R_Crypt.Common.Handlers
 
         public void StartBackgroundNotify()
         {
-            InternalTimer.Interval = TimeSpan.FromMilliseconds(50);
+            InternalTimer.Interval = TimeSpan.FromMilliseconds(200);
             InternalTimer.Tick += (sender, e) =>
             {
-                foreach (var file in CryptoFiles) 
-                    file.Update();
+                if (!IsUpdatingCollection)
+                {
+                    foreach (var file in CryptoFiles)
+                        file.Update();
+                }
             };
             InternalTimer.Start();
         }
